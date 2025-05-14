20a0844c8e85ff0c85f69d3cce3e6e54acb4626a
ansh-shipmnts <ansh.patel@shipmnts.com>
2025-05-15 00:20:28 +0530

    fix isbn

---
+++ migration/fix_isbn.py
@@ -0,0 +1,43 @@
+from HR.models import BookRecords
+import BulkUpload.BulkUploadFoEM.models as BulkUploadFoEM
+import BulkUpload.BulkUploadFoET.models as BulkUploadFoET
+import BulkUpload.BulkUploadFoLS.models as BulkUploadFoLS
+import BulkUpload.BulkUploadMaths.models as BulkUploadMaths
+import BulkUpload.BulkUploadScience.models as BulkUploadScience
+
+models = [
+    BulkUploadFoEM.ViewBook,
+    BulkUploadFoET.ViewBook,
+    BulkUploadFoLS.ViewBook,
+    BulkUploadMaths.ViewBook,
+    BulkUploadScience.ViewBook
+]
+
+def fix_isbn():
+    book_isbn_map = {}
+    duplicates = {}
+    for book in BookRecords.objects.filter(is_finalized=True):
+        uniq_key = f"{book.title}apas_deploy_split{book.month}apas_deploy_split{book.yr}"
+        if uniq_key not in book_isbn_map:
+            book_isbn_map[uniq_key] = book.isbn
+        else:
+            if book_isbn_map[uniq_key] != book.isbn:
+                if uniq_key not in duplicates:
+                    duplicates[uniq_key] = []
+                duplicates[uniq_key].append(book.isbn)
+                duplicates[uniq_key].append(book_isbn_map[uniq_key])
+    for title, dups in duplicates.items():
+        print(f"Title: {title}, ISBNs: {', '.join(list(set(dups)))}")
+    count = {}
+    for uniq_key, isbn in book_isbn_map.items():
+        title = uniq_key.split("apas_deploy_split")[0]
+        month = int(uniq_key.split("apas_deploy_split")[1])
+        year = int(uniq_key.split("apas_deploy_split")[2])
+        for model in models:
+            books = model.objects.filter(title=title, month=month, year=year)
+            books.update(isbn=isbn)
+            if str(model) not in count:
+                count[str(model)] = 0
+            count[str(model)] += books.count()
+    print(count)
+
